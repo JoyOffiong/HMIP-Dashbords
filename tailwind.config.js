ec7531c3a256a5696f36c0dc3858221a5403f522
JoyOffiong <blaqkly@gmail.com>
2024-02-16 11:41:30 +0100

    property pages initail commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './dashboard.html',  // Path to your HTML file
+    './Dashboards/dashboard.html',  // Path to your HTML file
     './HousingPriceIndex.html',
     './RealEstate.html',
     './MortgageBoard.html',
+    './PropertyPages/HousingData.html',
+    './PropertyPages/PropertyInfo.html',
+    './PropertyPages/MarketDirectory1.html',
+    './PropertyPages/DetailedMortgagaCalculator.html',
+    './PropertyPages/MarketDirectory2.html',
     './src/**/*.js', // Path to your JavaScript files
     './src/**/*.css', // Path to your CSS files
+    "./node_modules/tw-elements/dist/js/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'confortaa': ['Comfortaa', 'sans-serif'],
+      },
+    },
     screens: {
       'md': '768px', // Medium screens
       'lg': '1024px', // Large screens
