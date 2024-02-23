12b9ea9f383b1412878d4fa4a6d9b5e2a31762b4
JoyOffiong <blaqkly@gmail.com>
2024-02-23 12:00:12 +0100

    new changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './Dashboards/dashboard.html',  // Path to your HTML file
-    './HousingPriceIndex.html',
-    './RealEstate.html',
-    './MortgageBoard.html',
-    './PropertyPages/HousingData.html',
-    './PropertyPages/PropertyInfo.html',
-    './PropertyPages/MarketDirectory1.html',
-    './PropertyPages/DetailedMortgagaCalculator.html',
-    './PropertyPages/MarketDirectory2.html',
-    './src/**/*.js', // Path to your JavaScript files
     './src/**/*.css', // Path to your CSS files
+    './src/**/*.html', // Path to your CSS files
     "./node_modules/tw-elements/dist/js/**/*.js"
   ],
   theme: {
